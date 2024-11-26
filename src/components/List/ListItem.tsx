7a99e672ae16d86f47d46404f2658f9fab96d8b6
João Lameira <jmrlameira@gmail.com>
2024-11-26 23:29:46 +0000

    listitem selected

--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -3,17 +3,19 @@ import React from "react";
 import { ListItemProps } from "~/components/List/types";
 import Link from "next/link";
 import { cn } from "~/utils";
-import { useSearchParams } from "next/navigation";
+import { useParams, useSearchParams } from "next/navigation";
 
 const ListItem = ({ title, description, videoId }: ListItemProps) => {
   const searchParams = useSearchParams();
+  const pageParams = useParams();
   const params = new URLSearchParams(searchParams);
   const nextLink = () => `/video/${videoId}?${params.toString()}`;
 
   return (
     <Link
       className={cn(
-        "bg-neutral-800 p-4 rounded-lg space-y-2 block hover:ring-2 hover:ring-gray-400 transition-all duration-100 hover:bg-neutral-700"
+        "bg-neutral-800 p-4 rounded-lg space-y-2 block hover:ring-2 hover:ring-gray-400 transition-all duration-100 hover:bg-neutral-700",
+        pageParams.videoId === videoId && "bg-neutral-700 ring-gray-400 ring-2"
       )}
       href={nextLink()}
     >
